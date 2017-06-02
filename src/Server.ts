0e77d40309651195a4ada4251447da79c571b164
natsuki <natsuki.i@live.jp>
2017-06-03 00:23:20 +0900

    WebSocketでEcho

--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,20 @@
-import * as express from 'express'
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
-let app = express();
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server , path: '/socket'});
+
+wss.on('connection', (ws: WebSocket) => {
+    console.log(ws);
+    ws.on('message', (message) => {
+        wss.clients.forEach((o: WebSocket) => {
+            o.send(message);
+        });
+        console.log('received: %s', message)
+    });
+});
 
 app.use(express.static('public'));
 
@@ -8,6 +22,6 @@ app.get('*', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("server listening on port 3000");
 });
